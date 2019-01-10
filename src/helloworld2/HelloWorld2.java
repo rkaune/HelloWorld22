58893eba0b49c14fb81e097ce0fc90c323f5eeec
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2019-01-10 09:38:11 -0500

    Second stuff.

--- src/helloworld2/HelloWorld2.java
+++ src/helloworld2/HelloWorld2.java
@@ -16,7 +16,7 @@ public class HelloWorld2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ahmed likes PCs");
+        System.out.println("Ahmed likes PCs, but not Macs :b");
     }
     
 }
